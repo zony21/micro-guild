28b06927b791494f692f4be60a17895a3a118736
kaito <mokos.cook@gmail.com>
2022-11-25 14:34:48 +0900

    tagマネージャー修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,11 @@ import TagManager from 'react-gtm-module'
 import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+
   useEffect(() => {
-    TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_TAGMANAGER })
-  }, [])
+    TagManager.initialize({ gtmId: [process.env.NEXT_PUBLIC_TAGMANAGER] });
+  }, []);
+
   return (
     <>
       <Head>
