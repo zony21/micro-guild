2eb8e436115aded85a2a78a83f29a8e259a5f970
kaito <mokos.cook@gmail.com>
2022-11-25 15:11:59 +0900

    metaタグらへんの修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,23 +3,10 @@ import store from '../app/store'
 import '../styles/globals.scss'
 import '../styles/header.scss'
 import "../styles/swiper/style.css"
-import Head from 'next/head'
-import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta content="通常料金無料の求人サイト。SNSのように手軽に求人情報を投稿できます。" name="Description" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
-        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#faf9f8" />
-        <meta name="theme-color" content="#faf9f8"></meta>
-        <meta name="google-site-verification" content="d4ZbSEaezTdFZDMlHqEKKC12iSwTY6-pey0Xq0fGMbo" />
-      </Head>
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, {Html, Head, Main, NextScript} from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from "react";
 
 export default class MyDocument extends Document {
@@ -6,20 +6,29 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <meta content="通常料金無料の求人サイト。SNSのように手軽に求人情報を投稿できます。" name="Description" />
+                    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+                    <link rel="manifest" href="/favicons/site.webmanifest" />
+                    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+                    <meta name="msapplication-TileColor" content="#faf9f8" />
+                    <meta name="theme-color" content="#faf9f8"></meta>
+                    <meta name="google-site-verification" content="d4ZbSEaezTdFZDMlHqEKKC12iSwTY6-pey0Xq0fGMbo" />
                     {/* 1: Google Tag Manager ---------------------------- */}
                     <script dangerouslySetInnerHTML={{
                         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PQXSBWQ');`,
-                    }}/>
+                    }} />
                     {/* -------------------------------------------------- */}
                 </Head>
                 <body>
                     {/* 2: Google Tag Manager ---------------------------- */}
                     <noscript dangerouslySetInnerHTML={{
                         __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PQXSBWQ" height="0" width="0" style="display:none;visibility:hidden" />`,
-                    }}/>
+                    }} />
                     {/* -------------------------------------------------- */}
-                    <Main/>
-                    <NextScript/>
+                    <Main />
+                    <NextScript />
                 </body>
             </Html>
         );
